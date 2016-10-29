fb233a7e9e799de3174e68a6e769872326f7fb30
lawn <bloom.all.over.12@gmail.com>
2016-10-30 00:33:47 +0900

    Alucard Hotplug: Disable by default

--- arch/arm/mach-msm/alucard_hotplug.c
+++ arch/arm/mach-msm/alucard_hotplug.c
@@ -73,7 +73,7 @@ static struct hotplug_tuners {
 	bool suspended;
 } hotplug_tuners_ins = {
 	.hotplug_sampling_rate = 50,
-	.hotplug_enable = 1,
+	.hotplug_enable = 0,
 	.min_cpus_online = 1,
 	.maxcoreslimit = 4,
 	.maxcoreslimit_sleep = 1,
